fcb77e896b1dcc06ed5a93551ec20c87ce7d7dc9
Jake Stewart <jxs1170@cs.bham.ac.uk>
2019-10-26 17:19:26 +0100

    Implemented discord side of ping#

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const client = new Discord.Client()
 const fs = require('fs')
 const http = require('http')
 
+var commandQueue = []
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'})
-  res.write('Hello World!')
   console.log('Url used: ' + req.url)
+  if (req.url === '/pop') {
+    if (commandQueue.length > 0) {
+      res.write(commandQueue.shift())
+    } else {
+      res.write('Empty')
+    }
+  } else if (req.url === '/ping') {
+    client.channels.get('637648713421946882').send('Pong!');
+  } else {
+    res.write('Hello World!')
+  }
   res.end()
 }).listen(8080)
 
